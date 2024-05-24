e8ea34520868db4af3cc9358fb8c04728f0b7429
username <M.Hijazi@Nascode.com>
2024-05-24 16:22:58 +0300

    edit hero section

---
+++ client/pages/about.tsx
@@ -0,0 +1,124 @@
+{/* <section id={styles["feature"]} className={`container pt-5 `}>
+<div className={styles["fe-box"]}>
+  <Image
+    src="/products/f1.png"
+    alt="free shipping"
+    width={100}
+    height={100}
+  />
+  <h6>Free Shipping</h6>
+</div>
+
+<div className={styles["fe-box"]}>
+  <Image
+    src="/products/f2.png"
+    alt="Online Order"
+    width={100}
+    height={100}
+  />
+  <h6>Online Order</h6>
+</div>
+
+<div className={styles["fe-box"]}>
+  <Image
+    src="/products/f3.png"
+    alt="Save Money"
+    width={100}
+    height={100}
+  />
+  <h6>Save Money</h6>
+</div>
+
+<div className={styles["fe-box"]}>
+  <Image
+    src="/products/f4.png"
+    alt="Promotions"
+    width={100}
+    height={100}
+  />
+  <h6>Promotions</h6>
+</div>
+
+<div className={styles["fe-box"]}>
+  <Image
+    src="/products/f5.png"
+    alt="Happy Sell"
+    width={100}
+    height={100}
+  />
+  <h6>Happy Sell</h6>
+</div>
+
+<div className={styles["fe-box"]}>
+  <Image
+    src="/products/f6.png"
+    alt="F24/7 Support"
+    width={100}
+    height={100}
+  />
+  <h6>F24/7 Support</h6>
+</div>
+</section> */}
+
+
+
+
+/* products  */
+// #feature .fe-box{
+//     width: 160px;
+//     text-align: center;
+//     padding: 25px 15px;
+//     box-shadow: 20px 20px 34px rgba(0, 0, 0, 0.03);
+//     border: 1px solid #ccd8e7;
+//     border-radius: 4px;
+//     margin: 15px 0;
+//   }
+  
+//   #feature .fe-box:hover {
+//     box-shadow: 10px 10px 54px rgba(70, 62, 221, 0.1);
+//   }
+  
+//   #feature .fe-box h6{
+//     display: in-block;
+//     padding: 9px 8px 6px 8px;
+//     line-height: 1;
+//     border-radius: 4px;
+//     color: #086381;
+//     background-color: #fddde4;
+//   }
+  
+//   #feature .fe-box img{
+//     width: 100%;
+//     margin-bottom: 10px;
+      
+//   }
+    
+//   #feature{
+//     display: flex;
+//     align-items: center;
+//     justify-content: space-between;
+//     flex-wrap: wrap;
+//     margin-top: 5rem;
+//   }
+  
+//   #feature .fe-box:nth-child(2) h6{
+//     background-color: #cdebbc;
+//   }
+  
+  
+//   #feature .fe-box:nth-child(3) h6{
+//     background-color: #d1e8f2;
+//   }
+  
+//   #feature .fe-box:nth-child(4) h6{
+//     background-color: #cdd4f8;
+//   }
+  
+//   #feature .fe-box:nth-child(5) h6{
+//     background-color: #f6dbf6;
+//   }
+  
+//   #feature .fe-box:nth-child(6) h6{
+//     background-color: #fff2e5;
+//   }
+  
