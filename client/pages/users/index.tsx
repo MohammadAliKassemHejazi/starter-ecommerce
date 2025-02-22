c09bbe81eb0e8f8c2e7451b120fd83f66f758672
Mohammad <mohammad_hejazii@outlook.com>
2025-02-22 20:21:24 +0100

    adding routes

--- client/pages/users/index.tsx
+++ client/pages/users/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { fetchUsersByCreator, deleteUser, usersSelector } from "@/store/slices/myusersSlice";
+import { fetchUsersByCreator, deleteUser, usersSelector } from "@/store/slices/myUsersSlice";
 import Swal from "sweetalert2";
 import { useAppDispatch } from "@/store/store";
 
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -6,5 +6,7 @@ import storeRouter from "./store.route";
 import utileRouter from "./utile.route";
 import paymentRouter from "./payment.route";
 import cartRouter from "./cart.route"
-import orderRouter  from "./order.route"
-export { orderRouter,cartRouter,userRouter, authRouter, articleRouter,shopRouter,storeRouter,utileRouter,paymentRouter};
+import orderRouter from "./order.route"
+import categoriesRouter from "./category.route"
+import usersRouter from "./users.route"
+export { orderRouter,cartRouter,userRouter, authRouter, articleRouter,shopRouter,storeRouter,utileRouter,paymentRouter,categoriesRouter,usersRouter};
